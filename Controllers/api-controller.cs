4c7352dd3bf9558bdb97b80055fa6af1c0a3cb76
sekeko <fernando.sequeira@gmail.com>
2017-07-02 21:32:07 -0600

    upload to azure from api controller

--- Controllers/api-controller.cs
+++ Controllers/api-controller.cs
@@ -1,8 +1,12 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.File;
 
 namespace vuenetcore.Controllers
 {
@@ -15,6 +19,44 @@ namespace vuenetcore.Controllers
         {
             var msg = new { Message = "Hello SEKEKO World" };
             return this.Ok(msg);
+        }
+
+        [Route("UploadMultipleFiles")]
+        [HttpPost]
+        public IActionResult UploadMultipleFiles(List<IFormFile> files)
+        {
+            //Do something with the files here. 
+            return BadRequest();
+        }
+
+        [Route("UploadSingleFile")]
+        [HttpPost]
+        public IActionResult UploadSingleFile(IFormFile oneFile)
+        {
+
+            PostToAzureStorage(oneFile);
+
+            return Ok();
+        }
+
+        private void PostToAzureStorage(IFormFile oneFile){
+            //Connect to Azure
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=taxportal;AccountKey=P2+0Y0ltoflrcO4b8f7TFwneuAdn4c85h/VxAfPrsZgxNCgMyIQHOU/oQEKgt0C4DOeFsoWBVe6hGs4W9iqH9w==;EndpointSuffix=core.windows.net");
+
+            // Create a reference to the file client.
+            CloudFileClient azureFileClient = storageAccount.CreateCloudFileClient(); 
+
+            // Get a reference to the file share we created previously.
+            CloudFileShare share = azureFileClient.GetShareReference("taxfiles");     
+            CloudFileDirectory rootDir = share.GetRootDirectoryReference();
+            CloudFileDirectory sampleDir = rootDir.GetDirectoryReference("files");
+            CloudFile file = sampleDir.GetFileReference(oneFile.FileName);
+
+            Stream fileStream = oneFile.OpenReadStream();
+
+            file.UploadFromStreamAsync(fileStream);
+            fileStream.Dispose();
+
         }
     }
 }
